b11813a6b4fb7de9c92ec7a593487ebb6704337e
Malachai Frazier <malachaifrazier@Malachais-MBP.lan>
2024-05-01 10:32:55 -0400

    chore/npm install and whitespace cleanup

--- src/consts.ts
+++ src/consts.ts
@@ -29,15 +29,15 @@ export const PROJECTS: Metadata = {
 };
 
 export const SOCIALS: Socials = [
-  { 
+  {
     NAME: "twitter-x",
     HREF: "https://twitter.com/markhorn_dev",
   },
-  { 
+  {
     NAME: "github",
     HREF: "https://github.com/markhorn-dev"
   },
-  { 
+  {
     NAME: "linkedin",
     HREF: "https://www.linkedin.com/in/markhorn-dev",
   }
