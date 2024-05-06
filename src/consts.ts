c1dea2cc6d72dd68ecedcbaf5c670bf37d9558b5
Malachai Frazier <malachaifrazier@Malachais-MacBook-Pro.local>
2024-05-06 19:58:06 -0400

    public images updated with CC logos, founder letter edited, consts updated

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 import type { Site, Metadata, Socials } from "@types";
 
 export const SITE: Site = {
-  NAME: "Comic Collab",
+  NAME: "ComicCollab",
   EMAIL: "mal@noonthirty.one",
   NUM_POSTS_ON_HOMEPAGE: 0,//3,
   NUM_WORKS_ON_HOMEPAGE: 0,//2,
@@ -10,23 +10,23 @@ export const SITE: Site = {
 
 export const HOME: Metadata = {
   TITLE: "Home",
-  DESCRIPTION: "Astro Nano is a minimal and lightweight blog and portfolio.",
+  DESCRIPTION: "Join ComicCollab to navigate the comic creation journey with fellow artists and writers. Discover collaboration opportunities, streamline project management, and produce stunning comics.",
 };
 
-export const BLOG: Metadata = {
-  TITLE: "Blog",
-  DESCRIPTION: "A collection of articles on topics I am passionate about.",
-};
+// export const BLOG: Metadata = {
+//   TITLE: "Blog",
+//   DESCRIPTION: "A collection of articles on topics I am passionate about.",
+// };
 
-export const WORK: Metadata = {
-  TITLE: "Work",
-  DESCRIPTION: "Where I have worked and what I have done.",
-};
+// export const WORK: Metadata = {
+//   TITLE: "Work",
+//   DESCRIPTION: "Where I have worked and what I have done.",
+// };
 
-export const PROJECTS: Metadata = {
-  TITLE: "Projects",
-  DESCRIPTION: "A collection of my projects, with links to repositories and demos.",
-};
+// export const PROJECTS: Metadata = {
+//   TITLE: "Projects",
+//   DESCRIPTION: "A collection of my projects, with links to repositories and demos.",
+// };
 
 export const SOCIALS: Socials = [
   {
